7620ea4472bc3314ce8bc910713562525155b43c
Pierrette Lo <lpi@ohsu.edu>
2020-10-07 21:32:48 -0700

    first commit

---
+++ initialize_cv.R
@@ -0,0 +1,10 @@
+# code for starting a new CV
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Pierrette Lo",
+  data_location = "data/",
+  pdf_location = "",
+  html_location = "",
+  source_location = "",
+  use_network_logo = FALSE
+)
